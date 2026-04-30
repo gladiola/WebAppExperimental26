fdff5ec40a413f603b69bda5d57b56fe98f52b12
John O'Keefe-Odom <joo313@g.harvard.edu>
2026-04-30 18:10:58 -0400

    Automated commit from MSBuild

--- WebAppExperimental26/Models/Settings/AzureADSettings.cs
+++ WebAppExperimental26/Models/Settings/AzureADSettings.cs
@@ -11,12 +11,11 @@
         public required string Authority { get; set; }
         public required string Instance {  get; set; }
         public required string Domain { get; set; }
-
         public required string TenantId { get; set; }
         public required string ClientId { get; set; }
         public required List<ClientCredential> ClientCredentials { get; set; }
-        public required string CallbackPath { get; set; }
-        public required string SignedOutCallbackPath { get; set; }
+        public string CallbackPath { get; set; } = "/signin-oidc";
+        public string SignedOutCallbackPath { get; set; } = "/signout-callback-oidc";
     }
 }
 
--- WebAppExperimental26/Models/User/UserClaims.cs
+++ WebAppExperimental26/Models/User/UserClaims.cs
@@ -5,49 +5,35 @@ namespace WebAppExperimental26.Models.User
 {
     public class UserClaims
     {
-
-        /// <summary>
-        /// Variable to hold user identification data from JWT claims for the class to work from.
-        /// </summary>
-        //public required IEnumerable<System.Security.Claims.Claim> Claims { get; set; }
-
         /// <summary>
         /// Value that represents a session from the sid claim.
         /// </summary>
-        public required string Sid { get; set; }
+        public string? Sid { get; set; }
 
         /// <summary>
         /// Represents a user's id from the oid claim.
         /// </summary>
-        public required string Oid { get; set; }
+        public string? Oid { get; set; }
 
         /// <summary>
         /// Represents a user's name from the name claim.
         /// </summary>
-        public required string Name { get; set; }
+        public string? Name { get; set; }
 
         /// <summary>
         /// Represents a user's roles from the roles claim.
         /// </summary>
-        public required string[] Roles { get; set; }
+        public string[]? Roles { get; set; }
 
         /// <summary>
         /// Represents a user's email from the email claim.
         /// </summary>
-        public required string Email { get; set; }
-
+        public string? Email { get; set; }
 
         /// <summary>
         /// Constructor to neutralize and load values from a User.Claims in Razor.
         /// Class to hold user identification data from JWT claims
         /// </summary>
-        public UserClaims() {
-
-
-
-        }
-
-
-
+        public UserClaims() { }
     }
 }
